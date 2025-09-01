0168a60d5a06259d1dd916866d658650b76ad85f
Thamirys001 <yrsuga@gmail.com>
2025-09-01 20:12:48 -0300

    array.py

---
+++ array.py
@@ -0,0 +1,24 @@
+#lista de nomes
+listaNomes = ["Thamirys", "Mauro", "Igor", "Amanda", "Wellington"]
+print("primeiro nome:", listaNomes[0])
+print("segundo nome:", listaNomes[1])
+print("terceiro nome:", listaNomes[2])
+print("quarto nome:", listaNomes[3])
+print("quinto nome:", listaNomes[4])
+print("QUEM ESTÁ NA LISTA:")
+nome_procurado = "Thamirys"
+if nome_procurado in listaNomes:
+    print(f"{nome_procurado} está na lista.")
+    nome_procurado = "Mauro"
+if nome_procurado in listaNomes:
+    print(f"{nome_procurado} está na lista.")
+    nome_procurado = "Igor"
+if nome_procurado in listaNomes:
+    print(f"{nome_procurado} está na lista.")
+    nome_procurado = "Amanda"
+if nome_procurado in listaNomes:
+    print(f"{nome_procurado} está na lista.")
+    nome_procurado = "Wellington"
+if nome_procurado in listaNomes:
+    print(f"{nome_procurado} está na lista.")
+
